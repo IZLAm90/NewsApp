c234f005bad9f28ec5c2fa39120d60121ac2fecd
islam <isalm9090@gmail.com>
2023-08-15 20:52:35 +0300

    unit test for useCase

--- domain/src/test/java/com/example/domain/NewsUseCaseTest.kt
+++ domain/src/test/java/com/example/domain/NewsUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.example.domain
 
+import com.app.data.remote.Constants
 import com.app.data.remote.UserApi
 import com.example.data.model.NewAppData
 import com.example.data.model.NewData
@@ -10,9 +11,12 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -34,37 +38,59 @@ class NewsUseCaseTest {
         MockitoAnnotations.openMocks(this)
         api = mock(UserApi::class.java)
         appRepository = mock(NewsRepo::class.java)
-        appUseCase = mock(NewsUseCase::class.java)
+        appUseCase = NewsUseCase(appRepository)
     }
 
 
-//    @Test
-//    fun `getNews success with data`() = runBlockingTest {
-//        val fakeData = getFakeData()
-//        val fakeResponse = createFakeSuccessResponse(fakeData)
-//
-//        `when`(
-//            appRepository.getHeadLines(
-//                com.app.data.remote.Constants.PrefKeys.APP_KEY,
-//                "n"
-//            )
-//        ).thenReturn(flow { emit(fakeResponse) } )
-//        val result = mutableListOf<Response<NewAppData>>()
-//
-//         appUseCase.getHeadLines(
-//            com.app.data.remote.Constants.PrefKeys.APP_KEY,
-//            "n"
-//        ).collect(){result.add(it)}
-//
-//        // Verify that the emitted data matches the fake data
-//        assert(result.size == 1)
-//        assert(result[0] == fakeData)
-//    }
+    @Test
+    fun `getNews success with data`() = runBlockingTest {
+        val fakeData = getFakeData()
+        val fakeResponse = createFakeSuccessResponse(fakeData)
 
-    fun createFakeSuccessResponse(data: NewAppData): Flow<Response<NewAppData>> {
-        return flow {
-            emit(Response.success(data))
+        `when`(
+            appRepository.getHeadLines(
+                com.app.data.remote.Constants.PrefKeys.APP_KEY,
+                "n"
+            )
+        ).thenReturn(flow { emit(fakeResponse) } )
+        val result = mutableListOf<NewAppData?>()
+
+         appUseCase.getHeadLines(
+            com.app.data.remote.Constants.PrefKeys.APP_KEY,
+            "n"
+        ).collect(){result.add(it)}
+
+        // Verify that the emitted data matches the fake data
+        assert(result.size == 1)
+        assert(result[0] == fakeData)
+    }
+
+    @Test
+    fun `getNews failed with serverError`() = runBlockingTest {
+        val fakeResponse =  Response.error<NewAppData>(500, "{\"message\":\"Internal Server Error\" }".toResponseBody())
+
+        `when`(
+            appRepository.getHeadLines(
+                com.app.data.remote.Constants.PrefKeys.APP_KEY,
+                "n"
+            )
+        ).thenReturn(flow { emit(fakeResponse) } )
+        val result = mutableListOf<NewAppData?>()
+
+        // Verify that the emitted data matches the fake data
+
+        try{
+            appUseCase.getHeadLines(
+                com.app.data.remote.Constants.PrefKeys.APP_KEY,
+                "n"
+            ).single()
+        }catch (e:Throwable){
+            Assert.assertEquals(Constants.ERROR_API.SERVER_ERROR, e.message)
         }
+    }
+    fun createFakeSuccessResponse(data: NewAppData): Response<NewAppData> {
+        return Response.success(data)
+
     }
     fun getFakeData(): NewAppData {
         return NewAppData(
